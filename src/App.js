b983675bc1a3374784f51703a35ed92418ec0e4b
ashr-aful007 <ashraful99980@gmail.com>
2022-09-20 19:43:43 +0600

    Explore component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 77777;
+const singr = {name: 'Dr. Mahfuzur rahaman', job: 'singre'};
+const singr2 = {name: 'Eva Rahman', job: 'singre'}
+
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'white'
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Person></Person>
+     <Person></Person>
+     <Person></Person>
+     <h5>New Component. YAY</h5>
+     <Friend></Friend>
+     <Friend></Friend>
+     <Friend></Friend>
     </div>
   );
 }
+
+function Person(){
+  return (
+    <div className='person'>
+    <h1>Sakib AL Hasan</h1>
+    <p>Profession: Cricket</p>
+  </div>
+  )
+}
+
+function Friend(){
+  return (
+    <div className='container'>
+      <h3>Name: Ajay Devgun</h3>
+      <p>Job: maramari</p>
+    </div>
+  )
+}
+
 
 export default App;
